05cd735efc1e431d49645dfa644dcc6f6a430255
Eliub Rangel <eliubr47@gmail.com>
2023-05-03 21:30:45 -0600

    se trabajo arreglos en la Api

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -16,29 +16,49 @@ namespace Csharp_Apis.Controllers
         {
             MathResponse Result = new MathResponse()
             {
-                Numero1=Numero1,
-                Numero2=Numero2,
-                Operation="Sum",
+                Numero1 = Numero1,
+                Numero2 = Numero2,
+                Operation = "Sum",
                 Result = Numero1 + Numero2
             };
-            return Ok (Result);
+            return Ok(Result);
         }
         [HttpGet]
         [Route("substraction")]
-
         public ActionResult GetResta(int Numero1, int Numero2)
         {
             MathResponse Result = new MathResponse()
             {
-                Numero1=Numero1,
-                Numero2=Numero2,
-                Operation="substraction",
+                Numero1 = Numero1,
+                Numero2 = Numero2,
+                Operation = "substraction",
                 Result = Numero1 - Numero2
             };
-            return Ok (Result);
+            return Ok(Result);
         }
-        
 
-        
+        [HttpGet]
+        [Route("MathArray")]
+        public ActionResult GetArray(int Numero)
+        {
+            if (Numero < 1 || Numero > 10)
+            {
+                return BadRequest("El numero debe ser mayor que 1 o menor que 10");
+            }
+            int[] Array = new int[Numero];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Array[i] = i;
+
+            }
+            MathResponse Result = new MathResponse()
+            {
+                Operation = "Array",
+                Result = Array
+            };
+            return Ok(Result);
+        }
+
+
     }
 }
