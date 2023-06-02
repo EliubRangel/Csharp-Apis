a1984c6aaecf5a5b39807334e35ddcc9097558c0
Eliub Rangel <eliubr47@gmail.com>
2023-06-01 21:18:50 -0600

    se modifico api, para eliminar y modoficar

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Csharp_Apis.Context;
+using Csharp_Apis.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Csharp_Apis.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ClientesController:Controller
+    public class ClientesController : Controller
     {
         private readonly FirstDbContext dbContext;
 
@@ -17,7 +18,7 @@ namespace Csharp_Apis.Controllers
         {
             this.dbContext = dbContext;
         }
-        
+
         [HttpGet]
         public ActionResult Get()
         {
@@ -25,5 +26,41 @@ namespace Csharp_Apis.Controllers
             return Ok(clientes);
         }
 
+        [HttpPost]
+        public ActionResult NuevoCliente(Clientes cliente)
+        {
+            //validar el obj cliente
+            dbContext.Clientes.Add(cliente);
+            dbContext.SaveChanges();
+            return Ok(cliente);
+        }
+
+        [HttpPut]
+        public ActionResult ModificarCliente(Clientes cliente)
+        {
+            var cte = dbContext.Clientes.FirstOrDefault(x => x.Id == cliente.Id);
+            cte.Nombre = cliente.Nombre;
+            cte.Apellido = cliente.Apellido;
+            cte.FechaNacimiento = cliente.FechaNacimiento;
+            cte.Factura = cliente.Factura;
+            cte.Telefono = cliente.Telefono;
+
+            dbContext.Update(cte);
+            dbContext.SaveChanges();
+            return Ok(cte);
+        }
+        [HttpDelete]
+        public ActionResult EliminarCliente(int Id)
+        {
+            var cte =dbContext.Clientes.FirstOrDefault(x=> x.Id == Id);
+            if(cte == null)
+            {
+               return NotFound("No se encontro el cliente con el Id");
+            }
+            dbContext.Remove(cte);
+            dbContext.SaveChanges();
+            return Ok(cte);
+            
+        }
     }
 }
